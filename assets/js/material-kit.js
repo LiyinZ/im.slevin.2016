eeafccd8749ed18311e906b5b7fe1c8d32a2a03c
Slevin Zhang <4ce111@gmail.com>
2016-05-11 19:04:00 -0400

    Remove unused js. Add favicon

--- assets/js/material-kit.js
+++ assets/js/material-kit.js
@@ -1,9 +1,10 @@
 var transparent = true;
 
-var transparentDemo = true;
 var fixedTop = false;
 
 var navbar_initialized = false;
+
+var activeHash = '';
 
 $(document).ready(function(){
 
@@ -14,13 +15,6 @@ $(document).ready(function(){
     //  Activate the Tooltips
     $('[data-toggle="tooltip"], [rel="tooltip"]').tooltip();
 
-    // Activate Datepicker
-    if($('.datepicker').length != 0){
-        $('.datepicker').datepicker({
-             weekStart:1
-        });
-    }
-
     // Activate Popovers
     $('[data-toggle="popover"]').popover();
 
@@ -29,6 +23,25 @@ $(document).ready(function(){
       interval: 400000
     });
 
+  $(window).on('scroll', materialKit.checkScrollForTransparentNavbar);
+
+  $('.scroll').click(function(ev) {
+    ev.preventDefault();
+    if (activeHash != this.hash) {
+      var dest = 0;
+      var heightDiff = $(document).height() - $(window).height();
+      if ($(this.hash).offset().top > heightDiff) {
+        dest = heightDiff - 70;
+      } else {
+        dest = $(this.hash).offset().top - 70;
+      }
+      // go to destination
+      $('html, body').animate({
+        scrollTop: dest
+      }, 400, 'swing');
+      activeHash = this.hash;
+    }
+  });
 });
 
 materialKit = {
@@ -48,48 +61,12 @@ materialKit = {
                     $('.navbar-color-on-scroll').addClass('navbar-transparent');
                 }
             }
-    }, 17),
-
-    initSliders: function(){
-        // Sliders for demo purpose
-        $('#sliderRegular').noUiSlider({
-            start: 40,
-            connect: "lower",
-            range: {
-                min: 0,
-                max: 100
-            }
-        });
-
-        $('#sliderDouble').noUiSlider({
-            start: [20, 60] ,
-            connect: true,
-            range: {
-                min: 0,
-                max: 100
-            }
-        });
-    }
+    }, 17)
 }
 
 
 var big_image;
 
-materialKitDemo = {
-    checkScrollForParallax: debounce(function(){
-        var current_scroll = $(this).scrollTop();
-
-        oVal = ($(window).scrollTop() / 3);
-        big_image.css({
-            'transform':'translate3d(0,' + oVal +'px,0)',
-            '-webkit-transform':'translate3d(0,' + oVal +'px,0)',
-            '-ms-transform':'translate3d(0,' + oVal +'px,0)',
-            '-o-transform':'translate3d(0,' + oVal +'px,0)'
-        });
-
-    }, 6)
-
-}
 // Returns a function, that, as long as it continues to be invoked, will not
 // be triggered. The function will be called after it stops being called for
 // N milliseconds. If `immediate` is passed, trigger the function on the
