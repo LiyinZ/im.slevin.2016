989e84f617a14ed69d2b43b41d532e4f962fd145
Slevin Zhang <4ce111@gmail.com>
2016-05-12 12:55:44 -0400

    Add build script

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,11 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var nano = require('gulp-cssnano');
-var purify = require('gulp-purifycss');
-var uncss = require('gulp-uncss');
+var prefix = require('gulp-autoprefixer');
+var uglify = require('gulp-uglify');
+var htmlmin = require('gulp-htmlmin');
+var rename = require('gulp-rename');
+
 
 gulp.task('bs', function() {
   return gulp.src('./assets/bs/**/*.scss')
@@ -17,20 +20,36 @@ gulp.task('sass', function() {
   return gulp.src('./assets/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./assets/css'));
-})
+});
 
 gulp.task('css', function() {
   return gulp.src('./assets/css/*.css')
-    .pipe(concat('pure.css'))
-    // .pipe(concat('main.css'))
-    // .pipe(uncss({
-    //   html: ['index.html'],
-    //   javascript: ['./assets/js/*.js']
-    // }))
-    .pipe(purify(['./assets/js/*.js', './index.html']))
+    .pipe(concat('all.css'))
+    .pipe(prefix())
+    .pipe(gulp.dest('./assets/dist'));
+});
+
+gulp.task('js', function() {
+  return gulp.src(['./assets/js/material.min.js', './assets/js/material-kit.js'])
+    .pipe(concat('all.js'))
     .pipe(gulp.dest('./assets/dist'));
 });
 
 gulp.task('watch:sass', function() {
   gulp.watch('./assets/sass/**/*.scss', ['sass']);
 });
+
+gulp.task('dist', function() {
+  gulp.src('./assets/dist/all.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./assets/dist'));
+
+  gulp.src('./assets/dist/all.css')
+    .pipe(nano())
+    .pipe(gulp.dest('./assets/dist'));
+
+  return gulp.src('index.dev.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(rename('index.html'))
+    .pipe(gulp.dest('./'));
+})
